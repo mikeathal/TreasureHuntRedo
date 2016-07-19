aae52fbdc5b9784913a42d750b938dd09208f73c
Ryan McMahon <mcmahonryan1@mac.com>
2016-07-19 18:28:03 -0400

    The redone version of the game, should function normally as the last one did, first push

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GetComponent<Rigidbody2D>().velocity = new Vector2 (0f, 0f);
+
+
+		if (Input.GetKey (KeyCode.W)) {
+
+			GetComponent<Rigidbody2D> ().velocity += new Vector2 (0f, 300f) * Time.deltaTime;
+
+		}
+
+
+		if (Input.GetKey (KeyCode.S)) {
+
+			GetComponent<Rigidbody2D> ().velocity += new Vector2 (0f, -300f) * Time.deltaTime;
+
+		}
+
+
+		if (Input.GetKey (KeyCode.A)) {
+
+			GetComponent<Rigidbody2D> ().velocity += new Vector2 (-300f, 0f) * Time.deltaTime;
+
+		}
+
+		if (Input.GetKey (KeyCode.D)) {
+
+			GetComponent<Rigidbody2D> ().velocity += new Vector2 (300f, 0f) * Time.deltaTime;
+
+		}
+
+
+	}
+}
---
+++ Assets/Scripts/Treasure.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI ;
+
+public class Treasure : MonoBehaviour {
+	bool didPlayerWin = false ;
+	public Text WinText;
+	public Transform WinPos;
+	public Text MistyText ;
+	public Text SleepyText ;
+	public Text WideEyesText ;
+	public Text RainbowsText ;
+	// Use this for initialization
+
+	// Update is called once per frame
+	void Update () {
+		if ((WinPos.position - transform.position).magnitude < 2f) {
+			
+			if (Input.GetKeyDown (KeyCode.Space)) {
+				didPlayerWin = true;
+			}
+
+
+		}
+		if (didPlayerWin) {
+			WinText.text = "Nice! You made it to Jenna's party!";
+
+		}
+		if (WinPos.position.y < -36.2721f) {
+			MistyText.text = "Yooooo what's uuuuuup dude! \nOh maaan Jenna's party? Umm... \nWe know it's not like, in any of the \n4 2 rOoms... we're having a LAN party with the dudes in those rooms hahaha \nBest of luck yo!";
+
+		}
+		if (WinPos.position.x < -34f) {
+			SleepyText.text = "Mmmmmggghhhhhhhh... huh? \n'Z puhrty mgggghhhhh 'z east mmmmgggghhhh.";
+
+		}
+		if (WinPos.position.y > 37.373f) {
+			WideEyesText.text = "OMG HOW ARE YOU?!!?! IT'S BEEEEEEEN SO LONG SINCE I'VE SEEEEEEEN YOU!! \nCOME GIMME A HUG!! WHAT?! PARTY?! \nALL I REMEMBER IS IT'S IN AND EEEEEEEeeeeeEEEEEEEVEN DORM!!";
+
+		}
+		if (WinPos.position.x > 36.647f) {
+			RainbowsText.text = "Shhh, quiet down--can you see the walls breathing? \nReality itself is so...colorful...I'm so feeling at one with the carpet right now. \nOh a party? All I can think about the room was the curls in the room number.";
+
+		}
+
+	}
+}
