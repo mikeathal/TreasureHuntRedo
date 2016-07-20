65a84328a3c39edd993c43a3439d8320563bc036
Mike Thal <mikeathal@gmail.com>
2016-07-19 22:24:43 -0400

    Attempted code for playing audio clip on spacebar.  Will return to this tomorrow.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,9 +3,13 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
 
+	public AudioSource Knocking;
+
+
 	// Use this for initialization
 	void Start () {
-	
+		var audioClip = Resources.Load<AudioClip>("Knocking.wav");
+		Knocking.clip = audioClip;
 	}
 	
 	// Update is called once per frame
@@ -38,7 +42,10 @@ public class Player : MonoBehaviour {
 			GetComponent<Rigidbody2D> ().velocity += new Vector2 (300f, 0f) * Time.deltaTime;
 
 		}
-
+			
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			Knocking.Play();
+		}
 
 	}
 }
--- Assets/Scripts/Treasure.cs
+++ Assets/Scripts/Treasure.cs
@@ -14,6 +14,7 @@ public class Treasure : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
+
 		if ((WinPos.position - transform.position).magnitude < 2f) {
 			
 			if (Input.GetKeyDown (KeyCode.Space)) {
