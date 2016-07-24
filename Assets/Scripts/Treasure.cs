10407b1a4ed074cc61bca490bec24a04099f3cf7
Mike Thal <mikeathal@gmail.com>
2016-07-24 12:18:15 -0400

    Added player slowdown for misty. Also still try to fix win condition code

--- Assets/Scripts/Treasure.cs
+++ Assets/Scripts/Treasure.cs
@@ -3,8 +3,8 @@ using System.Collections;
 using UnityEngine.UI ;
 
 public class Treasure : MonoBehaviour {
-	bool didPlayerWin = false ;
-	bool WinDoorUnlocked = false;
+	bool didPlayerWin;
+	bool WinDoorUnlocked;
 	public Rigidbody2D PlayerSpeed;
 	public SpriteRenderer PlayerSprite;
 
@@ -14,51 +14,67 @@ public class Treasure : MonoBehaviour {
 	public Text WinText;
 	public Transform WinPos;
 
-	bool MistyVisited = false;
+	bool MistyVisited;
 	public Text MistyText;
 	public SpriteRenderer Misty;
 	public Transform MistyPos;
 
-	bool SleepyVisited = false;
+	bool SleepyVisited;
 	public Text SleepyText;
 	public SpriteRenderer Sleepy;
 	public Transform SleepyPos;
 
-	bool WideEyesVisited = false;
+	bool WideEyesVisited;
 	public Text WideEyesText;
 	public SpriteRenderer WideEyes;
 	public Transform WideEyesPos;
 
-	bool RainbowsVisited = false;
+	bool RainbowsVisited;
 	public Text RainbowsText;
 	public SpriteRenderer Rainbows;
 	public Transform RainbowsPos;
 
 	// Use this for initialization
 
+	void Start (){
+		
+		WinDoorUnlocked = false;
+		MistyVisited = false;
+		SleepyVisited = false;
+		WideEyesVisited = false;
+		RainbowsVisited = false;
+		didPlayerWin = false;
+
+	}
+
+
 	// Update is called once per frame
 	void Update () {
-		
+
+//		if (MistyVisited = true) {
+//			Debug.Log ("WTF MISTY?");
+//		}
+
 		// Checking if Player Won and has visited all rooms
-		if ( (WinPos != null) && ((WinPos.position - PlayerPos.position).magnitude < 2.5f) && (Input.GetKeyDown (KeyCode.Space)) &&
-			(WinDoorUnlocked = true) ) {
-
-			Debug.Log ("#Winning");
-			WinText.enabled = true;
-			didPlayerWin = true;
+		if ((WinPos != null) && ((WinPos.position - PlayerPos.position).magnitude < 2.5f) && (Input.GetKeyDown (KeyCode.Space))) {
+		
+			if (WinDoorUnlocked == true) {
+				WinText.text = "Nice! You made it to Jenna's party!";
+				didPlayerWin = true;
+			} else {
+				WinText.text = "Hmmm, this door's locked";	
+			}
 		}
-		if (didPlayerWin) {
-			WinText.text = "Nice! You made it to Jenna's party!";
-		}
-
-
 
 		// Checking if player has visited a room
 		if ( ((MistyPos.position-PlayerPos.position).magnitude < 2.5f) && (Input.GetKeyDown(KeyCode.Space)) ) {
-			Debug.Log("Misty Ran");
+			Debug.Log((MistyPos.position-PlayerPos.position).magnitude);
 			Misty.enabled = true;
 			MistyText.text = "Yooooo what's uuuuuup dude! \nOh maaan Jenna's party? Umm... We know it's not like, in any of the 4 2 rOoms... we're having a LAN party with the dudes in those rooms hahaha \nBest of luck yo!";
 			MistyVisited = true;
+
+			// Slow Player down
+			PlayerSpeed.drag = 140f ;
 		}
 
 		if ( ((SleepyPos.position-PlayerPos.position).magnitude < 2.5f) && (Input.GetKeyDown(KeyCode.Space)) ) {
@@ -73,7 +89,9 @@ public class Treasure : MonoBehaviour {
 			WideEyes.enabled = true;
 			WideEyesText.text = "OMG HOW ARE YOU?!?! IT'S BEEEN SO LONG SINCE I'VE SEEEEEN YOU!! \nCOME GIMME A HUG!! WHAT?! JENNA'S PARTY?! \nIT'S IN AND EEEEEEEeeeeeEEEEEEEVEN DORM!! Sleepy Brett might know something in West Hall\nTAKE THIS TO GET THERE FAAASTER!!";
 			WideEyesVisited = true;
-			PlayerSpeed.drag = 20f ;
+
+			// Give player speed boost
+			PlayerSpeed.drag = 20f;
 
 		}
 		if ( ((RainbowsPos.position-PlayerPos.position).magnitude < 2.5f) && (Input.GetKeyDown(KeyCode.Space)) ) {
@@ -84,8 +102,8 @@ public class Treasure : MonoBehaviour {
 		}
 
 		if ( (MistyVisited = true) && (SleepyVisited = true) && (WideEyesVisited = true) && (RainbowsVisited = true) ) {
+			Debug.Log ("DoorUnlocked");
 			WinDoorUnlocked = true;
 		}
-
 	}
 }
