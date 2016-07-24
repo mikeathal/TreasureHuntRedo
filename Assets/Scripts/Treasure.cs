aff4fb1a0841203cb22193f6745e23e52dbb19e2
Mike Thal <mikeathal@gmail.com>
2016-07-23 21:09:31 -0400

    Edited hints so that they're behind certain doors

--- Assets/Scripts/Treasure.cs
+++ Assets/Scripts/Treasure.cs
@@ -32,37 +32,38 @@ public class Treasure : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-		if (WinPos != null) {
-			if ((WinPos.position - transform.position).magnitude < 2f) {
-			
-				if (Input.GetKeyDown (KeyCode.Space)) {
+		if ( (WinPos != null) && ((WinPos.position - PlayerPos.position).magnitude < 2.5f) && (Input.GetKeyDown (KeyCode.Space)) ) {
+					Debug.Log ("#Winning");
+					WinText.enabled = true;
 					didPlayerWin = true;
 				}
-			}
-		}
 
 
 		if (didPlayerWin) {
 			WinText.text = "Nice! You made it to Jenna's party!";
 
 		}
-		if ( ((MistyPos.position-PlayerPos.position).magnitude < 1f) && (Input.GetKeyDown(KeyCode.Space)) ) {
+		if ( ((MistyPos.position-PlayerPos.position).magnitude < 2.5f) && (Input.GetKeyDown(KeyCode.Space)) ) {
 			Debug.Log("Misty Ran");
 			Misty.enabled = true;
 			MistyText.text = "Yooooo what's uuuuuup dude! \nOh maaan Jenna's party? Umm... We know it's not like, in any of the 4 2 rOoms... we're having a LAN party with the dudes in those rooms hahaha \nBest of luck yo!";
 
 		}
-		if ( ((SleepyPos.position-PlayerPos.position).magnitude < 1f) && (Input.GetKeyDown(KeyCode.Space)) ) {
+		if ( ((SleepyPos.position-PlayerPos.position).magnitude < 2.5f) && (Input.GetKeyDown(KeyCode.Space)) ) {
 			Debug.Log("Sleepy Ran");
 			Sleepy.enabled = true;
 			SleepyText.text = "Mmmmmggghhhhhhhh... huh? \n'Z puhrty mgggghhhhh 'z east mmmmgggghhhh.";
 
 		}
-		if (WinPos.position.y > 37.373f) {
+		if ( ((WideEyesPos.position-PlayerPos.position).magnitude < 2.5f) && (Input.GetKeyDown(KeyCode.Space)) ) {
+			Debug.Log ("WideEyes Ran");
+			WideEyes.enabled = true;
 			WideEyesText.text = "OMG HOW ARE YOU?!!?! IT'S BEEEEEEEN SO LONG SINCE I'VE SEEEEEEEN YOU!! \nCOME GIMME A HUG!! WHAT?! PARTY?! \nALL I REMEMBER IS IT'S IN AND EEEEEEEeeeeeEEEEEEEVEN DORM!!";
 
 		}
-		if (WinPos.position.x > 36.647f) {
+		if ( ((RainbowsPos.position-PlayerPos.position).magnitude < 2.5f) && (Input.GetKeyDown(KeyCode.Space)) ) {
+			Debug.Log ("Rainbows Ran");
+			Rainbows.enabled = true;
 			RainbowsText.text = "Shhh, quiet down--can you see the walls breathing? \nReality itself is so... colorful... I'm so feeling at one with the carpet right now. \nOh a party? All I can think about the room was the curls in the room number.";
 
 		}
